0accdda2ddbef96e5f3f5ee11d4e67d04804b8d5
Jim Shield <gothicjim@gmail.com>
2025-05-02 11:36:54 +0100

    version bump
    `2.0.04`

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 name "Jim_Bridge"
 author "Jimathy"
-version "2.0.03"
+version "2.0.04"
 description "Framework Bridge By Jimathy"
 fx_version "cerulean"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
